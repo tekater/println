bd2646844021843731964b696fc4065c8ecd1eb7
tekater <120014627+tekater@users.noreply.github.com>
2022-12-21 21:21:37 +0900

    Add files via upload

---
+++ main.java
@@ -0,0 +1,32 @@
+
+public class Main {
+  public static void main(String[] args) {
+//  System.out.print("Hello, world!");
+//	System.out.println();
+//	System.out.println("21_12_2022");
+//	System.out.println("I love animals!");
+//	System.out.println("Let's check on the animals...");
+//	System.out.println("The deer looks fine.");
+//	System.out.println("The bat looks happy.");
+//	System.out.println("The lion looks healty.");
+    System.out.println("""
+	I love animals!
+	Let`s check on the animals...
+	The deer looks fine.
+	The bat looks happy.
+	The lion looks Healty.
+	""");
+  }
+}
+// Int numberOfStudents = 5;
+// Int numberOfStudents;
+// numberOfStudents = 5;
+// String fruitName = "apple";
+// Разные регистры. fruit и Fruit
+// String FruitName = "banana"; 
+// var language = "Java";
+// var 1language = "java"; *не работает, не корректно
+// class,static,public,int
+// String var = 
+
+
